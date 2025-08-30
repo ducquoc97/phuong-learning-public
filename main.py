79578713b338439a18c6ecd43820394a6946ab43
Quoc <ducquoc2211@gmail.com>
2025-08-30 12:07:23 +0700

    3rd commit

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 print("Hello world")
 print("Learning")
+
+# generate a long function
+def long_function_name(var_one, var_two, var_three, var_four):
+    print("This is a long function")
+    print(var_one)
+    print(var_two)
+    print(var_three)
+    print(var_four)
